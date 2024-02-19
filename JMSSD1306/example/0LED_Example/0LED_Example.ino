3a4fb76bf14746953f30712895cfef1d1734f36c
= <ty10410205@gmail.com>
2024-02-19 16:40:18 +0800

    update example

--- JMSSD1306/example/0LED_Example/0LED_Example.ino
+++ JMSSD1306/example/0LED_Example/0LED_Example.ino
@@ -1,18 +1,14 @@
 #include <Arduino.h>
 #include <driver/i2c.h>
-#ifdef __cplusplus
+
 extern "C"{
-  #include "JM_SSD1306.h"
-}
-#endif
-
-
+#include "JM_SSD1306.h"
+};
 
 #define SDA_PIN 21
 #define SCL_PIN 22
 
 OLED oled;
-
 static esp_err_t i2c_master_init(int sda_io_num, int scl_io_num){
 
     int i2c_master_port = I2C_MASTER_NUM;
@@ -44,55 +40,50 @@ static esp_err_t init_i2c(void){
 
 }
 
-
-
-void displayTask(void *pvParameter){
-
-  vTaskDelay(100 / portTICK_PERIOD_MS);
-  oled.display_info(&oled);
+void Task_Display(void *pvParameter)
+{
   oled.clear(&oled);
-  int count = 0;
-  char buffer[50];
-  
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
-  
+  vTaskDelay(10 / portTICK_PERIOD_MS);
+  oled.display_info(&oled);
 
+  /* buffer for display character */
+  const char *student_id = "N961140XX";
+  char buf[50];
+
+  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  oled.clear(&oled);
+  
   while(1){
 
-    snprintf(buffer, sizeof(buffer), " Count: %d", count);
-    oled.print(&oled, 1, 1, buffer, 0);
-    count++;
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    snprintf(buf, sizeof(buf), " ID: %s", student_id);
+    oled.print(&oled, 1, 1, buf, 0);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
 
   }
+
 }
 
+void setup() 
+{
 
-void setup() {
-
-  vTaskDelay(2000 / portTICK_PERIOD_MS);
   Serial.begin(115200);
 
-  //初始化並且啟用ESP32 I2C通訊介面
+  /* 初始化並啟用esp32 i2c controller*/
   if(init_i2c() == ESP_OK){
-    Serial.println("i2c initialize sucessed");
-  }
-  
-  //初始化OLED暫存器配置
-  init_oled(&oled, SDA_PIN, SCL_PIN, 0x3c);
-  if((&oled) == NULL){
-    printf("\nInitialize OLED error !\r\n");
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println("Initialize i2c successed!");
   }
 
-  vTaskDelay(1000 / portTICK_PERIOD_MS);
+  /* 初始化oled */
+  if(init_oled(&oled, SDA_PIN, SCL_PIN, 0x3C) == ESP_OK){
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    Serial.println("Initialize OLED successed!");
+  }
 
-  Serial.printf("start oled task \r\n");
-  //建立並初始化顯示工作的Threads
-  xTaskCreate(displayTask, "TaskforDisplay", 1024 * 2, NULL, 1, NULL);
+  xTaskCreate(Task_Display, "Task_display", 1024 * 2, NULL, 1, NULL);
 
 }
 
 void loop() {
-  
 }
 
